c9641e68710f3c4b4b06a44b9f35c620316769d4
Sunny108700541 <sunnysinghchauhan082@gmail.com>
2025-04-26 22:27:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import InfoSection from './components/InfoSection';
 import FeaturesSection from './components/FeaturesSection';
 import FaqSection from './components/FaqSection';
 import Footer from './components/Footer';
-import TermsOfService from './pages/TermsOfService';
+import TermsOfService from './components/TermsOfService';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
