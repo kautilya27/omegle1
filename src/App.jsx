f0e78bdf3f3a23fb430cd927d1d114f3b979fc1e
Sunny108700541 <sunnysinghchauhan082@gmail.com>
2025-05-03 11:02:35 +0530

    Update App.jsx
    app.jsx change

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import MainCard from './components/MainCard';
 import InfoSection from './components/InfoSection';
@@ -5,11 +7,9 @@ import FeaturesSection from './components/FeaturesSection';
 import FaqSection from './components/FaqSection';
 import Footer from './components/Footer';
 import TermsOfService from './components/TermsOfService';
-//import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
-
-function App() {
+// Layout component to handle conditional rendering based on route
+function Layout() {
   const location = useLocation();
   const isTermsPage = location.pathname === '/terms-of-service';
 
@@ -17,16 +17,15 @@ function App() {
     <div style={{ backgroundColor: '#f9f6f2', minHeight: '100vh' }}>
       {!isTermsPage && <Header />}
       <main>
-        {!isTermsPage && (
-          <>
-            <MainCard />
-            <InfoSection />
-            <FeaturesSection />
-            <FaqSection />
-          </>
-        )}
         <Routes>
-          {/* Other routes */}
+          <Route path="/" element={
+            <>
+              <MainCard />
+              <InfoSection />
+              <FeaturesSection />
+              <FaqSection />
+            </>
+          } />
           <Route path="/terms-of-service" element={<TermsOfService />} />
         </Routes>
       </main>
@@ -35,12 +34,12 @@ function App() {
   );
 }
 
-function AppWrapper() {
+function App() {
   return (
     <Router>
-      <App />
+      <Layout />
     </Router>
   );
 }
 
-export default AppWrapper;
+export default App;
