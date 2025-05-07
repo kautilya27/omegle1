bc9ed0c613ca3fbee215b952248508a7f5aa99d6
kautilya27 <upkautilya@gmail.com>
2025-05-07 16:31:21 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -85,6 +85,8 @@ function Header() {
             fontWeight: 500,
             fontSize: 16,
           }}
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6382255537358474"
+     crossorigin="anonymous"></script>
         >
           online now
         </span>
